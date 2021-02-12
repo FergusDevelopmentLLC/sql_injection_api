c13f184c67bb3bf04a4ee44fd103f61a1bf446a7
Will Carter <will.carter@fergusllc.com>
2021-02-12 16:46:31 -0700

    refactor

--- server.js
+++ server.js
@@ -1,21 +1,47 @@
 const express = require('express')
 const utils = require('./utils')
+const bodyParser = require('body-parser');
+const sqlite3 = require('sqlite3').verbose()
 
 const app = express()
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }));
 
-utils.createSeedData()
+//utils.createSeedData()
 
-app.use('/players', (req, res, next) => {
-  
-  const players = utils.getPlayers()
-  
+let db
+
+app.get('/players', async (req, res, next) => {
+  db = new sqlite3.Database('player.db')
+  const players = await utils.getPlayers(db)
+  db.close()
   res.status(200).json(players)
 })
 
-app.use('/', (req, res, next) => {
+app.post('/playerByEmail', async (req, res, next) => {
+  db = new sqlite3.Database('player.db')
+  let emailAddress = req.body.email
+  const player = await utils.getPlayerByEmail(db, emailAddress)
+  db.close()
+  res.status(200).json(player)
+})
+
+app.post('/playerByEmailStrong', async (req, res, next) => {
+  db = new sqlite3.Database('player.db')
+  let emailAddress = req.body.email
+  const player = await utils.getPlayerByEmailStrong(db, emailAddress)
+  db.close()
+  res.status(200).json(player)
+})
+
+app.get('/', (req, res, next) => {
   res.status(200).json(`Hello from node-express. The current server date/time is: ${new Date()}`)
 })
 
 const server = app.listen(8888, () => {
   console.log('App listening at port %s', server.address().port)
 })
+
+// {
+//   "email": "jump23@bulls.com' union SELECT 1,sql,3 FROM sqlite_master WHERE type!='meta' AND sql NOT NULL AND name NOT LIKE 'sqlite_%' AND name = 'players';"
+// }
